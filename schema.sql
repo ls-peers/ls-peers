a0fb62e2a8588ae08c42d5e7f2061413f27b9e00
AlonsoLo <alonso.lobato.jimenez@gmail.com>
2021-12-27 18:35:32 -0500

    Small changes in db schema already agreed by both

--- schema.sql
+++ schema.sql
@@ -14,37 +14,35 @@ CREATE TABLE tracks (
 
 CREATE TABLE timezones (
   id serial PRIMARY KEY,
-  code varchar(5) NOT NULL,          -- e.g EST, CET, MIT,
-  name text,                         -- e.g Eastern Standard Time, Central European Time, Marquesas Islands Time
-  gmtoffset numeric(4,2) NOT NULL          -- e.g -5, 1, -9.50
+  code varchar(5) NOT NULL,
+  name text,
+  gmtoffset numeric(4,2)                                                                  -- Not in use for now
 );
 
-
 CREATE TABLE users (
-  id serial, -- change datatype from serial to: uuid DEFAULT uuid_generate_v4 ()
-  firstname varchar(25) NOT NULL CHECK (LENGTH(TRIM(BOTH ' ' FROM firstname)) > 0), -- should we use the checks in the form instead of here?
+  id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+  firstname varchar(25) NOT NULL CHECK (LENGTH(TRIM(BOTH ' ' FROM firstname)) > 0),       -- should we use the checks in the form instead of here?
   lastname varchar(25),
   slackname varchar(25) NOT NULL,
   email varchar(255) NOT NULL,
-  password char(60) NOT NULL, -- it seems that BCrypt always generates 60 character hashes
+  password char(60) NOT NULL,                                                             -- it seems that BCrypt always generates 60 character hashes
   course_id integer NOT NULL REFERENCES courses(id) ON DELETE CASCADE,
   track_id integer NOT NULL REFERENCES tracks(id) ON DELETE CASCADE,
   timezone_id integer NOT NULL REFERENCES timezones(id) ON DELETE CASCADE,
   about_me text,
   last_active timestamp,
   created_at timestamp NOT NULL DEFAULT NOW(),
-  updated_at timestamp,
-  PRIMARY KEY (id)
+  updated_at timestamp
 );
 
 CREATE TABLE preferences (
   id serial PRIMARY KEY,
-  preference varchar(255),                   -- e.g 'Preparing for interview assessment', 'Reviewing concepts', etc
-  category varchar(150)                      -- e.g 'Study', 'Project', 'Assessment', etc
+  preference varchar(255),
+  category varchar(150)
 );
 
 CREATE TABLE users_preferences (
   id serial PRIMARY KEY,
-  user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE, -- change datatype from integer to uuid
+  user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   preference_id integer NOT NULL REFERENCES preferences(id) ON DELETE CASCADE
 );
--- test_data.sql
+++ test_data.sql
@@ -8,16 +8,16 @@ INSERT INTO tracks (code, name)
   VALUES ('RB', 'Ruby'),
          ('JS', 'Javascript');
 
-INSERT INTO timezones (code, name, gmtoffset)
-  VALUES ('EST', 'Eastern Standard Time', -5),
-         ('CET', 'Central European Time', 1),
-         ('PST', 'Pacific Standard Time', -8);
+INSERT INTO timezones (code, name)
+  VALUES ('EST', 'Eastern Standard Time'),
+         ('CET', 'Central European Time'),
+         ('PST', 'Pacific Standard Time');
 
-INSERT INTO users (firstname, lastname, slackname, email, password,
+INSERT INTO users (id, firstname, lastname, slackname, email, password,
                    course_id, track_id, timezone_id, about_me)
-  VALUES ('Scott', 'Graham', 'Scott', 'scttgrhm7@gmail.com', 'ldfgkj78%^&appdKO039*',
+  VALUES ('fc10b881-d9a0-4ab1-a6fd-a102db188f49', 'Scott', 'Graham', 'Scott', 'scttgrhm7@gmail.com', 'ldfgkj78%^&appdKO039*',
           2, 1, 3, 'Finishing LS and planning to do capstone in May. I like surfing and thick grey socks'),
-         ('Alonso', 'Lobato', 'Alonso Lobato', 'alonzilj@gmail.com', 'ppdK78%^&aO039*ldfgkj',
+         ('fc10b881-d9a0-4ab1-a6fd-a102db188f50', 'Alonso', 'Lobato', 'Alonso Lobato', 'alonzilj@gmail.com', 'ppdK78%^&aO039*ldfgkj',
           1, 1, 1, 'About to finish back-end portion of Ruby track. I like canadian Christmas trees');
 
 INSERT INTO preferences (preference, category)
@@ -28,18 +28,19 @@ INSERT INTO preferences (preference, category)
          ('Working on a project', 'Project');
 
 INSERT INTO users_preferences(user_id, preference_id)
-  VALUES ('1',5), -- user id is of type serial for development purposes, later it'll became  uuid
-         ('1',4),
-         ('2',2),
-         ('2',4),
-         ('2',5);
+  VALUES ('fc10b881-d9a0-4ab1-a6fd-a102db188f49',5),
+         ('fc10b881-d9a0-4ab1-a6fd-a102db188f49',4),
+         ('fc10b881-d9a0-4ab1-a6fd-a102db188f50',2),
+         ('fc10b881-d9a0-4ab1-a6fd-a102db188f50',4),
+         ('fc10b881-d9a0-4ab1-a6fd-a102db188f50',5);
 
 -- Querying Database
 
-SELECT firstname, lastname, slackname, tracks.name, courses.code FROM users
-  INNER JOIN courses ON courses.id = users.course_id
-  INNER JOIN tracks ON tracks.id = users.track_id
-  INNER JOIN users_preferences ON users_preferences.user_id = users.id
-  INNER JOIN preferences  ON preferences.id = users_preferences.preference_id
+SELECT firstname, lastname, slackname, tracks.name, courses.code
+  FROM users
+    INNER JOIN courses ON courses.id = users.course_id
+    INNER JOIN tracks ON tracks.id = users.track_id
+    INNER JOIN users_preferences ON users_preferences.user_id = users.id
+    INNER JOIN preferences  ON preferences.id = users_preferences.preference_id
   WHERE preferences.category = 'Problem solving'
   ORDER BY last_active DESC;
